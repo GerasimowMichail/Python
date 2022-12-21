975951193bd49dc496d5f9f949c42b31995d53ec
Mischa <GerasimowMichail@yandex.ru>
2022-12-21 21:35:03 +0300

    Задание №9 и 10 переделаны

--- HomeWork_9/bot_commands.py
+++ HomeWork_9/bot_commands.py
@@ -29,78 +29,90 @@ def time_command(update: Update, context: CallbackContext):
     log(update, context)
     update.message.reply_text(f'{datetime.datetime.now().time()}')
 
-# /sum 12 25
+# /sum 12  25
+
 def sum_command(update: Update, context: CallbackContext):
     log(update, context)
     msg = update.message.text
     print(msg)
-    items = msg.split() # '/sum', '12+3j', '25'
+    items = msg.split() # '/sum', '12' " " '25'
     x = float(items[1])
     y = float(items[2])
     update.message.reply_text(f'{x} + {y} = {x + y}')
 
-# /sum 12 25
+# /deff 12+3j  25-16j - разность комплексных чисел
 def deff_command(update: Update, context: CallbackContext):
     log(update, context)
     msg = update.message.text
     print(msg)
-    items = msg.split() # '/sum', '12+3j', '25'
+    items = msg.split() # '/deff', '12+3j' " " '15+6j
     x = float(items[1])
     y = float(items[2])
     update.message.reply_text(f'{x} - {y} = {x - y}')
     
+    # /mult 12  25 - произведение комплексных чисел
 def mult_command(update: Update, context: CallbackContext):
     log(update, context)
     msg = update.message.text
     print(msg)
-    items = msg.split() # '/sum', '12+3j', '25'
+    items = msg.split() # '/mult', '12' " " '15'
     x = float(items[1])
     y = float(items[2])
     update.message.reply_text(f'{x} * {y} = {x * y}')
     
+    # /div 25  25
 def div_command(update: Update, context: CallbackContext):
     log(update, context)
     msg = update.message.text
     print(msg)
-    items = msg.split() # '/sum', '12+3j', '25'
+    items = msg.split() # '/div', '25' " " '25'
     x = float(items[1])
     y = float(items[2])
     update.message.reply_text(f'{x} / {y} = {x / y}')
 
 
-# /csum 12 25
+# /csum 12+3j  25-16j - сумма комплексных чисел
 def csum_command(update: Update, context: CallbackContext):
     log(update, context)
     msg = update.message.text
     print(msg)
-    items = msg.split() # '/sum', '12+3j', '25'
+    items = msg.split() # '/csum', '12+3j'  " "  '12+5j'
     x = complex(items[1])
     y = complex(items[2])
     update.message.reply_text(f'{x} + {y} = {x + y}')
     
+    
+    
+# /cdeff 12+3j  25-16j - разность комплексных чисел
+
 def cdeff_command(update: Update, context: CallbackContext):
     log(update, context)
     msg = update.message.text
     print(msg)
-    items = msg.split() # '/sum', '12+3j', '25'
+    items = msg.split() # '/cdeff', '12+3j'  " "  '25+16j'
     x = complex(items[1])
     y = complex(items[2])
     update.message.reply_text(f'{x} - {y} = {x - y}')
+
+
+# /cdiv 12+3j  25-16j - частное комплексных чисел
 
 def cdiv_command(update: Update, context: CallbackContext):
     log(update, context)
     msg = update.message.text
     print(msg)
-    items = msg.split() # '/sum', '12+3j', '25'
+    items = msg.split() # '/cdiv', '12+3j' " " '25+26j'
     x = complex(items[1])
     y = complex(items[2])
     update.message.reply_text(f'{x} / {y} = {x / y}')
-
+    
+    
+   # /cmult 12+3j  25-16j - произведение комплексных чисел
 def cmult_command(update: Update, context: CallbackContext):
     log(update, context)
     msg = update.message.text
     print(msg)
-    items = msg.split() # '/sum', '12+3j', '25'
+    items = msg.split() # '/cmult', '12+3j' " " '25-16j'
     x = complex(items[1])
     y = complex(items[2])
     update.message.reply_text(f'{x} * {y} = {x * y}')
